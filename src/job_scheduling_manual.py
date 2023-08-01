6e41f2c204c4de0c375c1cb7b01dc1467b48a239
jamesbradbury333 <jamesbradbury222@gmail.com>
2023-08-01 20:07:58 +0100

    Add a manual version of job_scheduling.py
    I created this version to get my head around the python-z3 api.
    Manually coding every step and rule helps you learn the API and
    how to set up the theorums in a SAT/SMT solver.

---
+++ src/job_scheduling_manual.py
@@ -0,0 +1,111 @@
+
+from z3 import Int
+from z3 import Optimize
+from z3 import And
+from z3 import Solver
+
+
+def main():
+    solver = Solver()
+    optimize = Optimize()
+    time_var = Int(f"time")
+
+    start_var_1 = Int(f"start{1}")
+    start_var_2 = Int(f"start{2}")
+    start_var_3 = Int(f"start{3}")
+    start_var_4 = Int(f"start{4}")
+    start_var_5 = Int(f"start{5}")
+    start_var_6 = Int(f"start{6}")
+    start_var_7 = Int(f"start{7}")
+    start_var_8 = Int(f"start{8}")
+    start_var_9 = Int(f"start{9}")
+    start_var_10 = Int(f"start{10}")
+
+    end_var_1 = Int(f"end{1}")
+    end_var_2 = Int(f"end{2}")
+    end_var_3 = Int(f"end{3}")
+    end_var_4 = Int(f"end{4}")
+    end_var_5 = Int(f"end{5}")
+    end_var_6 = Int(f"end{6}")
+    end_var_7 = Int(f"end{7}")
+    end_var_8 = Int(f"end{8}")
+    end_var_9 = Int(f"end{9}")
+    end_var_10 = Int(f"end{10}")
+
+    duration_var_1 = Int(f"duration{1}")
+    duration_var_2 = Int(f"duration{2}")
+    duration_var_3 = Int(f"duration{3}")
+    duration_var_4 = Int(f"duration{4}")
+    duration_var_5 = Int(f"duration{5}")
+    duration_var_6 = Int(f"duration{6}")
+    duration_var_7 = Int(f"duration{7}")
+    duration_var_8 = Int(f"duration{8}")
+    duration_var_9 = Int(f"duration{9}")
+    duration_var_10= Int(f"duration{10}")
+
+    solver.add(duration_var_1 == 10 + 1)
+    solver.add(duration_var_2 == 10 + 2)
+    solver.add(duration_var_3 == 10 + 3)
+    solver.add(duration_var_4 == 10 + 4)
+    solver.add(duration_var_5 == 10 + 5)
+    solver.add(duration_var_6 == 10 + 6)
+    solver.add(duration_var_7 == 10 + 7)
+    solver.add(duration_var_8 == 10 + 8)
+    solver.add(duration_var_9 == 10 + 9)
+    solver.add(duration_var_10 == 10 + 10)
+
+
+    solver.add(And(start_var_1 >= 0, start_var_1 < end_var_1))
+    solver.add(And(start_var_2 >= 0, start_var_2 < end_var_2))
+    solver.add(And(start_var_3 >= 0, start_var_3 < end_var_3))
+    solver.add(And(start_var_4 >= 0, start_var_4 < end_var_4))
+    solver.add(And(start_var_5 >= 0, start_var_5 < end_var_5))
+    solver.add(And(start_var_6 >= 0, start_var_6 < end_var_6))
+    solver.add(And(start_var_7 >= 0, start_var_7 < end_var_7))
+    solver.add(And(start_var_8 >= 0, start_var_8 < end_var_8))
+    solver.add(And(start_var_9 >= 0, start_var_9 < end_var_9))
+    solver.add(And(start_var_10 >= 0, start_var_10 < end_var_10))
+
+    solver.add(duration_var_1 == end_var_1 - start_var_1)
+    solver.add(duration_var_2 == end_var_2 - start_var_2)
+    solver.add(duration_var_3 == end_var_3 - start_var_3)
+    solver.add(duration_var_4 == end_var_4 - start_var_4)
+    solver.add(duration_var_5 == end_var_5 - start_var_5)
+    solver.add(duration_var_6 == end_var_6 - start_var_6)
+    solver.add(duration_var_7 == end_var_7 - start_var_7)
+    solver.add(duration_var_8 == end_var_8 - start_var_8)
+    solver.add(duration_var_9 == end_var_9 - start_var_9)
+    solver.add(duration_var_10 == end_var_10 - start_var_10)
+
+    solver.add(end_var_1 <= time_var)
+    solver.add(end_var_2 <= time_var)
+    solver.add(end_var_3 <= time_var)
+    solver.add(end_var_4 <= time_var)
+    solver.add(end_var_5 <= time_var)
+    solver.add(end_var_6 <= time_var)
+    solver.add(end_var_7 <= time_var)
+    solver.add(end_var_8 <= time_var)
+    solver.add(end_var_9 <= time_var)
+    solver.add(end_var_10 <= time_var)
+
+    optimize.minimize(time_var)
+    print(solver.check())
+    print(solver.model())
+   
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+
+
+if __name__ == "__main__":
+    main()
